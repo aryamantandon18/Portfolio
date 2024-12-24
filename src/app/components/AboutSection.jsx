ec3c82289a3c999a2caef636f3273ebc3ad753b7
AryamanTandon18 <tandon.aryaman1@gmail.com>
2024-12-24 21:41:21 +0530

    final

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -71,7 +71,7 @@ const AboutSection = () => {
             I am a full stack web developer with a passion for creating
             interactive and responsive web applications. I have experience
             working with JavaScript, React, Redux, Node.js, Express, MongoDB,PostgreSQL,
-            Sequelize, HTML, CSS, and Git at a start called HUBX. I am a quick learner and I am always
+            Sequelize, HTML, CSS & Github. I am a quick learner and I am always
             looking to expand my knowledge and skill set. I am a team player and
             I am excited to work with others to create amazing applications.
           </p>
